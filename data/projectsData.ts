2386ff1c51c8ea9bc891a87beea967b55cbd927e
TheGamingMousse <68484800+TheGamingMousse@users.noreply.github.com>
2024-11-20 17:17:31 -0800

    initial creation

---
+++ data/projectsData.ts
@@ -0,0 +1,12 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  // there's nothing here because we don't need this part
+]
+
+export default projectsData
