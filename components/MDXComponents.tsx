9ab1be6be08669967b26ff7381580e6d7886f813
Justin Ji <icedragon1689@gmail.com>
2024-12-12 11:17:42 -0800

    misc site changes
    removing some unnecessary stuff
    
    will try to get rid of analytics + newsletter later, but for now it stays

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,5 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
@@ -14,7 +13,6 @@ export const components: MDXComponents = {
   a: CustomLink,
   pre: Pre,
   table: TableWrapper,
-  BlogNewsletterForm,
   Spoiler,
   Quiz,
   QuizQuestion,
